67d75e5048d4e68e63636a31336a63ade598eaf2
anasrana <anas.rana@gmail.com>
2013-11-24 22:11:29 +0000

    updated example code

--- example_code.r
+++ example_code.r
@@ -14,7 +14,10 @@ m.cl <- 12:20
 fit.K <- StammMSEcv.K(g.dat, t.dat, k.states=k.stt, hat.m=13, n.core=50, l.pen=0, return.all=TRUE)
 fit.m <- StammMSEcv.m(g.dat, t.dat, m.cl=m.cl, k.states=k.stt, n.core=50, l.pen=0)
 
-StammMSEcvK.plot(fit.K$mse.cv, k.states=1:5)
-StammStab.plot(fit.m$fit, m.v=m.cl, m.init=2, k.states=k.stt)
+MSE.cv <- StammMSEcvK.plot(fit.K$mse.cv, k.states=k.stt)
+print(MSE.p)
+Stab.m <- StammStab.plot(fit.m$fit, m.v=m.cl, m.init=2, k.states=k.stt)
+print(Stab.m)
 i.k <- which(k.stt==fit.K$hat.K)
-StammStabPen.plot(fit.K$fit, fit.K$fit.cv[[i.k]], t.dat)
+Stab.l <- StammStabPen.plot(fit.K$fit, fit.K$fit.cv[[i.k]], t.dat)
+print(Stab.l)
